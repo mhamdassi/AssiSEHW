db95baf8a2d53f3ede0a292514c9520672aef058
Mohammed Assi <mhamd.assi@gmail.com>
2020-02-05 19:41:00 +0200

    Assiv1

---
+++ src/Lowclass.java
@@ -0,0 +1,14 @@
+
+public class Lowclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sum s = new Sum();
+		s.x=5;
+		s.y=6;
+		System.out.println(s.plus(s.getX(), s.getY()));
+		
+
+	}
+
+}
---
+++ src/Sum.java
@@ -0,0 +1,25 @@
+
+public class Sum {
+	int x,y,z;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	int plus(int x, int y) {
+		return z=getX() + getY();
+	}
+	
+
+}
